633834876e25b7248e3ddc7c2dbeefa1fccfc3ba
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-09-22 10:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,6 +25,11 @@ const getSum = () => {
 
     const table = document.querySelector("table");
     table.appendChild(totalRow);
+
+	const ansElement = document.createElement("div");
+	ansElement.id = "ans";
+	ansElement.textContent = `Total Price: Rs ${totalPrice.toFixed(2)}`;
+	document.body.appendChild(ansElement);
 };
 
 getSumBtn.addEventListener("click", getSum);
